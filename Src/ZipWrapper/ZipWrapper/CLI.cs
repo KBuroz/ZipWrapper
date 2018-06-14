6d22c1d9b687a6938acd5eb2cef9c40810e842f5
Kyle Burrows <kylbrrws@gmail.com>
2018-06-14 18:00:13 -0400

    Added the CLI class

---
+++ Src/ZipWrapper/ZipWrapper/CLI.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ZipWrapper
+{
+    class CLI
+    {
+        IZipShell _zipShell;
+
+        public CLI(IZipShell zipShell)
+        {
+            _zipShell = zipShell;
+        }
+
+        public void ParseCommand(string[] args)
+        {
+            
+        }
+    }
+}
