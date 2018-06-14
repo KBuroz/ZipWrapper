f9acc2326935aff0b8496d8a708e028075a34004
Kyle A. Burrows <KBurrows@skylinesoft.com>
2018-06-14 14:33:01 -0400

    Initial Add

---
+++ Src/ZipWrapper/ZipWrapper/IZipShell.cs
@@ -0,0 +1,10 @@
+﻿using System.IO.Compression;
+using System.Text;
+
+namespace ZipWrapper
+{
+    public interface IZipShell
+    {
+
+    }
+}
---
+++ Src/ZipWrapper/ZipWrapper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipWrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0) Help(args);
+            else if (args[0].ToUpper() == "EXTRACT") EXTRACT(args);
+            else if (args[0].ToUpper() == "CREATE") CREATE(args);
+            else if (args[0].ToUpper() == "HELP") Help(args);
+            else Help(args);
+        }
+
+        static void EXTRACT(string[] args)
+        {
+            ZipFile
+        }
+
+        static void CREATE(string[] args)
+        {
+
+        }
+
+        static void Help(string[] args)
+        {
+            Console.WriteLine("Examples:\r\n");
+            Console.WriteLine("  ZipWrapper Extract\r\n");
+            Console.WriteLine("  ZipWrapper Create\r\n");
+        }
+    }
+}
