c74925d28147a4447e3f3bd83daad7c80e82fbad
Kyle Burrows <kylbrrws@gmail.com>
2018-06-16 15:05:58 -0400

    Implemented CompressionLevel parser #10

---
+++ Src/ZipWrapper/ZipWrapper/CompressionLevelParser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Compression;
+
+namespace ZipWrapper
+{
+    public class CompressionLevelParser : IParser<CompressionLevel>
+    {
+        public CompressionLevel Parse(string arg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
