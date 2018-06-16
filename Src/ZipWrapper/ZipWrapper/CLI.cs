18838ede50d345ee020847dae7e7aafc964028ee
Kyle Burrows <kylbrrws@gmail.com>
2018-06-16 11:09:22 -0400

    Implemented extract

--- Src/ZipWrapper/ZipWrapper/CLI.cs
+++ Src/ZipWrapper/ZipWrapper/CLI.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace ZipWrapper
 {
@@ -15,7 +16,43 @@ namespace ZipWrapper
 
         public void ParseCommand(string[] args)
         {
-            
+            if(args.Length > 0)
+            {
+                var primaryArg = args[0].ToUpper();
+
+                switch(primaryArg)
+                {
+                    case "EXTRACT":
+                        // TODO: Validate the extract command args
+                        // CommandValidator.ValidateExtract(args);
+                        if (args.Length == 3)
+                        {
+                            _zipShell.ExtractToDirectory(args[1], args[2]);
+                        }
+                        else if(args.Length == 4)
+                        {
+                            throw new NotImplementedException("Different encodings are not yet implemented.");
+                            // _zipShell.ExtractToDirectory(args[1], args[2], encoding.Parse(args[3]) );
+                        }
+                        break;
+                    default:
+                        Help();
+                        break;
+                }
+            }
+            else
+            {
+                Help();
+            }
+        }
+
+        private void Help()
+        {
+            Console.WriteLine("====================================");
+            Console.WriteLine("==  Help  ==========================");
+            Console.WriteLine("====================================");
+
+
         }
     }
 }
